dff0d83fa33dc7a55065f6dbecde14f574b115b5
Yashsarode933 <yash@example.com>
2026-06-28 13:55:35 +0530

    Fix Jest TypeScript configuration

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,7 +4,17 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        target: 'ES2022',
+        module: 'CommonJS',
+        lib: ['ES2022'],
+        strict: true,
+        esModuleInterop: true,
+        skipLibCheck: true,
+        types: ['jest', 'node']
+      }
+    }],
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
   collectCoverageFrom: [
